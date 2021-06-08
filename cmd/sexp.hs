91b6a452a7d8a638d1c2cdb1b6148a9e0d347111
rohan <rd>
2021-06-09 09:56:01 +1000

    ~cmd/rw

--- cmd/sexp.hs
+++ cmd/sexp.hs
@@ -1,3 +1,5 @@
+import Control.Monad {- base -}
+
 import qualified Music.Theory.Opt as Opt {- hmt -}
 
 import Sound.SC3.Lisp.Haskell {- hsc3-lisp -}
@@ -10,17 +12,18 @@ opt =
   ,("mode","module","string","parser mode, module|expression")]
 
 help :: Opt.OptHelp
-help = ["hs-to-sexp [opt]"]
+help = ["hsc3-sexp haskell-to-lisp [opt]"]
 
 main :: IO ()
 main = do
-  (o,_) <- Opt.opt_get_arg True help opt
+  (o,a) <- Opt.opt_get_arg True help opt
   let usage = Opt.opt_error help opt
       mode x = case x of
                  "expression" -> hs_exp_to_lisp
                  "module" -> hs_to_lisp
                  _ -> usage
       table x = if x == "nil" then Nothing else Just x
+  when (a /= ["haskell-to-lisp"]) usage
   hs_to_lisp_f_io
     (mode (Opt.opt_get o "mode"))
     (table (Opt.opt_get o "name-rewrite-table"))
